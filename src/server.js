d5aaf69905c6d1bee8a9df113913ab37642f4c9d
klockett <kelockett@fullsail.edu>
2017-01-02 17:17:00 -0800

    Added Basic API route and Unit test

---
+++ src/server.js
@@ -0,0 +1,15 @@
+var express =  require('express');
+var body_parser = require('body-parser');
+var app = express();
+
+//Config
+var port = 3000;
+
+app.use('/api', require('../routes/api.js')(express));
+
+var server = app.listen(port, function(){
+  console.log('Server Active on', port);
+
+});
+
+module.exports = server;
